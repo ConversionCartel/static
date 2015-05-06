26c7f5f4d682b86ad4575cc0fbda2f7a2214d0ab
Mon Lu <mon@ave81.com>
2015-05-06 18:39:48 -0500

    Fixed up JS a bit

--- build/dist/avalon-school/js/functions.js
+++ build/dist/avalon-school/js/functions.js
@@ -2,17 +2,14 @@
 $(function(){
 	$('.js--menu').click(function(){
 		$('.ui--menu').toggleClass('is-active');
-		return false;
 	});
 
-	$('.sidebar-menu a').on('click', function() {
-		if ($('.sidebar-menu a').hasClass('is-active')) {
-			$('a').removeClass('is-active');
-		}
-		$(this).addClass('is-active');
+	/*$('.sidebar-menu a').on('click', function() {
+		$(this).hasClass('is-active') ? $(this).removeClass('is-active') : $(this).addClass('is-active');
 
 		return false;
-	});
+	});*/
+
 	$('.sidebar-toggle').on('click', function() {
 		$(this).toggleClass('toggle-is-active');
 		$('.sidebar-menu').toggleClass('sidebar-is-active');
